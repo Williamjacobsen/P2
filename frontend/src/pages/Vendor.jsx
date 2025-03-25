5a359ab25b1d64a79482a6b959544e1050f0ad19
William Jacobsen <villi05.v.j@gmail.com>
2025-03-25 10:03:13 +0100

    dadwa

---
+++ frontend/src/pages/Vendor.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Vendor.css";
+
+export default function Vendor() {
+  return (
+    <div>
+      <h1 className="gg" style={{ backgroundColor: "red" }}>
+        dghawidghawd
+      </h1>
+    </div>
+  );
+}
