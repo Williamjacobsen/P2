caa334dc557579533e1de3f812ce840e0ec8e28d
Kabuum <thomsenribermartin@gmail.com>
2025-03-25 20:56:36 +0100

    Made little scalable sorting cards, still styled ugly but i don't care right now

---
+++ frontend/src/pages/ProductCatalogue/Catalogue-Filter.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./Product-Card.css"
+
+function FilterOption({optionName, IsTitle = false}) {
+    if(IsTitle){
+        return(
+            <option value={optionName} hidden={true}>{optionName}</option>
+        )
+    }
+    return(
+        <option value={optionName}>{optionName}</option>
+    )
+}
+
+export default function CatalogueFilter({FilterName, FilterOptions}) {
+    return (
+        <div>
+            <label htmlFor="category"></label>
+            <select id="category" defaultValue="" className="SortBox">
+                <FilterOption optionName={FilterName} IsTitle={true} />
+                {FilterOptions.map((option) => (
+                    <option value={option}>{option}</option>
+                ))}
+            </select>
+        </div>
+    );
+}
--- frontend/src/pages/ProductCatalogue/Product-Catalogue.jsx
+++ frontend/src/pages/ProductCatalogue/Product-Catalogue.jsx
@@ -1,15 +1,40 @@
 import ProductCard from "./Product-Card";
 import "./Product-Card.css"
+import CatalogueFilter from "./Catalogue-Filter";
 
 export default function ProductCatalogue (){
+    const array = [
+        <ProductCard storeName="Shop 1" productName="Pants 1" price={100} imgURL="/Img/TestBillede.png" />,
+        <ProductCard storeName="Shop 2" productName="Pants 2" price={100} imgURL="/Img/TestBillede.png" />,
+        <ProductCard storeName="Shop 3" productName="Pants 3" price={100} imgURL="/Img/TestBillede.png" />,
+        <ProductCard storeName="Shop 4" productName="Pants 4" price={100} imgURL="/Img/TestBillede.png" />,
+        <ProductCard storeName="Shop 5" productName="Pants 5" price={100} imgURL="/Img/TestBillede.png" />,
+        <ProductCard storeName="Shop 6" productName="Pants 6" price={100} imgURL="/Img/TestBillede.png" />
+    ];
+    const genderOptionArray = [
+        'Male','Female'
+    ]
+    const clothingOptionArray = [
+        'T-shirt','Hoodie','Pants','Jeans'
+    ]
+    const priceOptionArray = [
+        'Highest Price','Lowest Price'
+    ]
+    const sizeOptionArray = [
+        'Small', 'Medium', 'Large',
+    ]
+
     return (
-        <div className="product-grid">
-            <ProductCard storeName="Shop 1" productName="Pants 1" price={100} imgURL="/Img/TestBillede.png" />
-            <ProductCard storeName="Shop 2" productName="Pants 2" price={100} imgURL="/Img/TestBillede.png" />
-            <ProductCard storeName="Shop 3" productName="Pants 3" price={100} imgURL="/Img/TestBillede.png" />
-            <ProductCard storeName="Shop 4" productName="Pants 4" price={100} imgURL="/Img/TestBillede.png" />
-            <ProductCard storeName="Shop 5" productName="Pants 5" price={100} imgURL="/Img/TestBillede.png" />
-            <ProductCard storeName="Shop 6" productName="Pants 6" price={100} imgURL="/Img/TestBillede.png" />
+        <div>
+            <div className="SortBoxes">
+                <CatalogueFilter FilterName={'Gender'} FilterOptions={genderOptionArray} />
+                <CatalogueFilter FilterName={'Product Type'} FilterOptions={clothingOptionArray} />
+                <CatalogueFilter FilterName={'Price'} FilterOptions={priceOptionArray} />
+                <CatalogueFilter FilterName={'Size'} FilterOptions={sizeOptionArray} />
+            </div>
+            <div className="product-grid">
+                {array}
+            </div>
         </div>
     )
 }
