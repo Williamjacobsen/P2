d930004b6ac59638081f6286235f2f7790b7492a
KingKaae <slindb24@student.aau.dk>
2025-04-03 12:26:39 +0200

    Header Test

---
+++ frontend/src/pages/Header/Header.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+export default function Layout() {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <nav className="flex justify-between items-center px-6 py-3 border-b shadow-md">
+      {/* Left Section - Website */}
+      <div className="flex items-center">
+        <span className="text-lf font-bold">Insert Cool Website Name</span>
+        <span className="mx-4 border-l h-6"></span> {/* Vertical Separator */}
+      </div>
+
+      {/* Center Section - Navigation Links */}
+      <ul className="flex space-x-6 text-sm">
+        <li><Link to="/" className="hover:underline">Home</Link></li>
+        <li><Link to="/products" className="hover:underline">Products</Link></li>
+        <li><Link to="/profile" className="hover:underline">Profile</Link></li>
+        <li><Link to="/help" className="hover:underline">Help</Link></li>
+        <li><Link to="/partners" className="hover:underline">For Partners</Link></li>
+      </ul>
+
+      {/*Right Section - Cart */}
+      <div className="flex items-center space-x-2">
+        <span className="text-sm font-bold">Cart</span>
+      </div>
+    </nav>
+  );
+};
