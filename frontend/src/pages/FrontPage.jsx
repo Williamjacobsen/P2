b5b4fd8961e4d93e5b203d749aa97d977d31e696
MunasarMohamed <mmam24@student.aau.dk>
2025-04-14 12:01:44 +0200

    update

--- frontend/src/pages/FrontPage.jsx
+++ frontend/src/pages/FrontPage.jsx
@@ -1,27 +1,35 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import './FrontPage.css';
 
 const FrontPage = () => {
-  return (
-    <section>
-    <div className="background">
-    <div className="content">
-      <h1>Aalborg <span>Clothing Shops</span></h1>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius earum animi eveniet perspiciatis numquam nobis possimus nihil molestias itaque, quae quidem facilis maiores ducimus adipisci nulla voluptate dolore laboriosam. Nisi?.</p>
-      <div class="buttons">
-        <button className="primary-btn">Shop Now</button>
-        <button className="secondary-btn">View Collection</button>
-      </div>
-      <div className="search-bar">
-      <input type="text" placeholder="Search for products..." />
-      <button>Search</button>
-    </div>
-    </div>
-  </div>
-   <div className="fp-products">
-    </div>
-    </section>
-  );
-};
+  const navigate = useNavigate();
+    
+      return (
+      <section>
+        <div className="background">
+        <div className="content">
+        <h1>Aalborg <span>Clothing Shops</span></h1>
+        <p>Lorem ipsum, dolor sit amet consectetur... </p>
+          
+        <div className="buttons">
+        <button 
+          className="primary-btn" 
+          onClick={() => navigate('/Product-Catalogue')}> Shop Now</button>
+        <button 
+          className="secondary-btn" 
+          onClick={() => navigate('/')}>View Profile</button>
+        </div>
 
+        <div className="search-bar">
+        <input type="text" placeholder="Search for products..." />
+        <button>Search</button>
+        </div>
+        </div>
+        </div>
+        <div className="fp-products"></div>
+        </section>
+      );
+    };
+    
 export default FrontPage;
