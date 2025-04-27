9e70d2146a613c3acb2f1fbe430d5f8fe402e29f
William Jacobsen <villi05.v.j@gmail.com>
2025-04-27 12:35:22 +0200

    it works locally now (vercel dev)

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,9 @@
 import express from "express";
 import cors from "cors";
-import pool from "../db.js";
-import serverless from "serverless-http";
+import pool from "./db.js";
 
 const app = express();
-/*const port = 3001;
+const port = 3001;
 
 app.use(
   cors({
@@ -15,22 +14,22 @@ app.use(express.json());
 
 app.use("/uploads", express.static("uploads"));
 
-import productImagesRoute from "../routes/product-images-example-for-martin.js";
+import productImagesRoute from "./routes/product-images-example-for-martin.js";
 app.use("/product-images", productImagesRoute);
 
-import addProductRoute from "../routes/add-product.js";
+import addProductRoute from "./routes/add-product.js";
 app.use("/add-product", addProductRoute);
 
-import profileRoute from "../routes/profile.js";
+import profileRoute from "./routes/profile.js";
 app.use("/profile", profileRoute);
 
-import vendorRoute from "../routes/vendor.js";
+import vendorRoute from "./routes/vendor.js";
 app.use("/vendor", vendorRoute);
 
-import productOrderRoute from "../routes/productOrder.js";
+import productOrderRoute from "./routes/productOrder.js";
 app.use("/productOrder", productOrderRoute);
 
-import payment from "../routes/payment.js";
+import payment from "./routes/payment.js";
 app.use("/checkout", payment);
 
 app.get("/test", (req, res) => {
@@ -126,10 +125,9 @@ app.get("/products", async (req, res) => {
     res.status(500).json({ error: "Failed to fetch products" });
   }
 });
-*/
 
-app.get("/test", (req, res) => {
-  res.send("API is working!");
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
 
-export default serverless(app);
+export default app;
