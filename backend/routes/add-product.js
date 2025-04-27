125d81bb8c85ac710d83746f5eb25a2677cf0f25
William Jacobsen <villi05.v.j@gmail.com>
2025-04-27 18:43:36 +0200

    .

--- backend/routes/add-product.js
+++ backend/routes/add-product.js
@@ -103,13 +103,11 @@ router.post("/", upload.array("images", 10), async (req, res) => {
     });
   } catch (error) {
     console.error("Error adding product:", error);
-    res
-      .status(500)
-      .json({
-        message: "Error adding product to database",
-        error: error,
-        req_body: req.body,
-      });
+    res.status(500).json({
+      message: "Error adding product to database",
+      error: error,
+      req_body: req.body,
+    });
   }
 });
 
