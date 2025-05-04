7b302db8e7bd86796b0d7f52dfe3439916fd5857
William Jacobsen <villi05.v.j@gmail.com>
2025-05-04 21:20:04 +0200

    .

---
+++ backend/routes/productID.js
@@ -0,0 +1,32 @@
+import express from "express";
+import pool from "../db.js";
+
+const router = express.Router();
+
+/** Gets all product data with related image paths and store name */
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const [result] = await pool.query(
+      `SELECT 
+           p2.Product.*, 
+           p2.Vendor.Name AS StoreName, 
+           p2.productimage.Path,
+           p2.Vendor.Address AS StoreAddress
+           FROM p2.Product
+           JOIN p2.Vendor ON p2.Product.StoreID = p2.Vendor.ID
+           LEFT JOIN p2.ProductImage ON p2.Product.ID = p2.ProductImage.ProductID
+           WHERE p2.Product.ID = ?;`,
+      [id]
+    );
+
+    res.status(200).json(result);
+  } catch (err) {
+    console.error("Error fetching product:", err);
+    res.status(500).json({ error: "Failed to fetch products" });
+  }
+});
+
+export default router;
