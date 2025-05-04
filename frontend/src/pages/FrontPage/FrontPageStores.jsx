310b46769b788581d3d5aaccae3b41a37500a5f3
William Jacobsen <villi05.v.j@gmail.com>
2025-05-04 22:28:17 +0200

    .

---
+++ frontend/src/pages/FrontPage/FrontPageStores.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "../SearchBar.css";
+
+export default function BoxShop() {
+  const [shops, setShops] = useState([]);
+
+  useEffect(() => {
+    const shopCircles = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_BACKEND_URL}/shopCircles`
+        );
+        if (!response.ok) {
+          throw new Error("Failed To Fetch ShopCircles");
+        }
+        const data = await response.json();
+        setShops(data);
+      } catch (err) {
+        console.log("error fetching shopCircles", err);
+      }
+    };
+    shopCircles();
+  }, []);
+
+  return (
+    <div className="box-shops">
+      {shops.map((shop) => (
+        <Link
+          to={`/Product-Catalogue?store=${encodeURIComponent(shop.Name)}`}
+          key={shop.id}
+          className="shop-circle"
+        >
+          {/*
+          <img src={shop.image} />
+          */}
+          <span>{shop.Name}</span>
+        </Link>
+      ))}
+    </div>
+  );
+}
