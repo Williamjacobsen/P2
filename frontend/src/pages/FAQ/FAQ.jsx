5f6b9479fb126dd0af8824dab561ec7e868f4f26
CarlGraversen <cgrave24@student.aau.dk>
2025-05-04 11:39:30 +0200

    faq

--- frontend/src/pages/FAQ/FAQ.jsx
+++ frontend/src/pages/FAQ/FAQ.jsx
@@ -1,29 +1,36 @@
-import React, { useState, useEffect } from "react";
-
+import React from "react";
 import "./FAQ.css";
 import useGetVendors from "../Profile/useGetVendors";
 
 export default function FAQ() {
-
   // Hooks
   const [isLoadingVendors, vendors] = useGetVendors();
 
   // Loading vendors
   if (isLoadingVendors) {
-    return (<>Loading vendors...</>);
+    return <>Loading vendors...</>;
   }
 
   return (
-    <>
-      <hr />
-      <div class="text-with-new-lines" >
-        {vendors[0].FAQ.replaceAll("newLineCharacter", "\n")}
+    <div className="faq-basic">
+      <h1>Frequently Asked Questions</h1>
+
+      {/* hardcoded FAQs for hjemmesiden */}
+      <div className="faq-item">
+        <div className="faq-question">What is the website's contact information?</div>
+        <div className="faq-answer">clothing@gmail.com and +45 12 12 12 12</div>
       </div>
-      <br />
-      <hr />
-      <div class="text-with-new-lines">
-        {vendors[1].FAQ.replaceAll("newLineCharacter", "\n")}
+      <div className="faq-item">
+        <div className="faq-question">Where are the stores located?</div>
+        <div className="faq-answer">Aalborg Central (example)</div>
       </div>
-    </>
+
+      {/* Vendor FAQs*/}
+      {vendors.map((vendor, index) => (
+        <div key={index} className="faq-item text-with-new-lines">
+          <div>{vendor.FAQ.replaceAll("newLineCharacter", "\n")}</div>
+        </div>
+      ))}
+    </div>
   );
 }
